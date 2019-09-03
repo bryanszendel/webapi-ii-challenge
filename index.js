847bb119863a5777c39ab4ae953079f6a2595df4
Bryan Szendel <bryan.szendel@gmail.com>
2019-09-03 14:01:57 -0600

    GET request to /api/posts/id retrieves a single post by id

--- index.js
+++ index.js
@@ -17,5 +17,20 @@ server.get('/api/posts', (req, res) => {
     })
 })
 
+server.get('/api/posts/:id', (req, res) => {
+  const postId = req.params.id;
+  data.findById(postId)
+    .then(result => {
+      if (result) {
+        res.status(200).json(result)
+      } else {
+        res.status(404).json({ message: "The post with the specified ID does not exist." })
+      }
+    })
+    .catch(error => {
+      res.status(500).json({ error: "The post information could not be retrieved." })
+    })
+})
+
 const port = 8000;
 server.listen(port, () => console.log(`\n Server running on port ${port}`))
